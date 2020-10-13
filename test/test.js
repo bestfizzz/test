6f273f7c1cdb6157db1b62ba2d3dcdd4ee099252
bestfizzz <52324074+bestfizzz@users.noreply.github.com>
2020-10-13 20:26:00 +0700

    h

--- test/test.js
+++ test/test.js
@@ -1,23 +1,22 @@
 //1
 var a1=[1,2,"a"]
-var a2=[1,3,"b"]
+var a2=[3,1,"b"]
 listmaker=(a1,a2)=>{
     let list=[]
-    for(i=0;i<=a1.lenght;i++){
-        if(!(a1[i] in a2)){
+    console.log(a1,a2)
+    for(i=0;i<a1.length;i++){
+        if(!(a2.includes(a1[i],0))){
             list.push(a1[i])
         }    
     }
-    for(i=0;i<=a1.lenght;i++){
-        if(!(a2[i] in a1)){
+    for(i=0;i<a2.length;i++){
+        if(!(a1.includes(a2[i],0))){
             list.push(a2[i])
-    }
-    for(i=0;i<=a1.lenght;i++){
-        if(list[i]===undefined){
-            list.splice(i,1)
-        }
-    }
+    }}
+    console.log(list);
 }
+
+listmaker(a1,a2)
 //2
 list2=[{name:"a",
     points:99,
@@ -32,17 +31,41 @@ gd:29,
 points:88,
 gd:39,
 },]
-for(i=0;i<=a1.lenght;i++){
-    if(list2[0].gd<list2[i].gd){
-        l=list2[0]
-        list2[0]=list2[i]
-        list2[i]=l
+for(i=0;i<list2.length;i++){
+    for(j=0;j<list2.length;j++){
+        if(list2[i].gd<list2[j].gd){
+            let temp = list2[i];
+            list2[i] = list2[j];
+            list2[j] = temp;;
+    }
+    
     }
 }
-for(i=0;i<=a1.lenght;i++){
-    list2[i].positions=i+1
+j=0
+for(i=list2.length;i>0;i--){
+    j=j+1
+    list2[i-1].positions=j
 }
-
+console.log(list2);
 fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple')
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(response =>response.json())
+    .then(data => {
+        console.log(data);
+        let x=0
+        wrong=(x)=>{
+            alert('wrong.')
+            return x=x+1
+        }
+        document.getElementById('difficulty').innerText='difficulty: '+data.results[x].difficulty
+        document.getElementById('category').innerText='category: ' + data.results[x].category
+        document.getElementById('quiz').innerText=data.results[x].question
+        document.getElementById('a').innerText=data.results[x].incorrect_answers[0]
+        document.getElementById('d').addEventListener('click',()=>{
+            alert('nice');
+            x=x+1
+        })
+        document.getElementById('b').innerText=data.results[x].incorrect_answers[1]
+        document.getElementById('c').innerText=data.results[x].incorrect_answers[2]
+        document.getElementById('d').innerText=data.results[x].correct_answer
+      ;
+    }).catch(err => console.error(err))
